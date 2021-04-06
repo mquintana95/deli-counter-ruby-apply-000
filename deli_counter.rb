0c2026318f501e9fa3f7795ba3c22b04f6152c08
Marcus Quintana <mq97325@gmail.com>
2021-04-06 13:25:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,12 +16,19 @@ def line(array)
   end
 end
 
-def take_a_number(array, name)
-  array.push(name)
-  position = array.index(name)
-  puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
-  return name, position 
-end 
+#def take_a_number(array, name)
+  #array.push(name)
+  #position = array.index(name)
+  #puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
+  #return name, position 
+#end
+  number = 0
+def take_a_number(array)
+  number += 1
+  array.push(number)
+  puts "Welcome #{number}"
+end
+
 
 def now_serving(array)
   if array.empty? == true 
